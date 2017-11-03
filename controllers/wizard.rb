62b4cfbb219af0d3a08adbad4ab7f3d83c7edcca
Angus McLeod <angus@mcleod.org.au>
2017-11-03 18:06:05 +0800

    bugfix

--- controllers/wizard.rb
+++ controllers/wizard.rb
@@ -1,8 +1,6 @@
 class CustomWizard::WizardController < ::ApplicationController
   prepend_view_path(Rails.root.join('plugins', 'discourse-custom-wizard', 'views'))
   layout 'wizard'
-
-  skip_before_action :check_xhr, :preload_json
   
   before_action :ensure_logged_in
   helper_method :wizard_page_title
