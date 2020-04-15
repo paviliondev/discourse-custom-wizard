57a19d4f23a6356d3b74caae76112a7d58fe4b0e
Angus McLeod <angus@mcleod.org.au>
2020-04-15 12:34:39 +1000

    Minor bugfixes

--- assets/javascripts/discourse/routes/admin-wizards-transfer.js.es6
+++ assets/javascripts/discourse/routes/admin-wizards-transfer.js.es6
@@ -4,5 +4,9 @@ import DiscourseRoute from "discourse/routes/discourse";
 export default DiscourseRoute.extend({
   model() {
     return CustomWizard.all();
+  },
+  
+  setupController(controller, model) {
+    controller.set('wizards', model)
   }
 });
--- lib/custom_wizard/log.rb
+++ lib/custom_wizard/log.rb
@@ -13,8 +13,8 @@ class CustomWizard::Log
   def self.create(message)
     log_id = SecureRandom.hex(12)
     
-    PluginStore.set('custom_wizard',
-      "log_#{log_id}",
+    PluginStore.set('custom_wizard_log',
+      log_id.to_s,
       {
         date: Time.now,
         message: message
@@ -24,8 +24,7 @@ class CustomWizard::Log
   
   def self.list_query
     PluginStoreRow.where("
-      plugin_name = 'custom_wizard' AND
-      key LIKE 'log_%' AND
+      plugin_name = 'custom_wizard_log' AND
       (value::json->'date') IS NOT NULL
     ").order("value::json->>'date' DESC")
   end
