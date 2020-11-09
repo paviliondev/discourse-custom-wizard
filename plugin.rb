0a542af7c6bf62e5ea13339cee9064236a342654
Angus McLeod <angus@mcleod.org.au>
2020-11-09 14:33:46 +1100

    Bump version

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-custom-wizard
 # about: Create custom wizards
-# version: 0.3
+# version: 0.5.0
 # authors: Angus McLeod
 # url: https://github.com/angusmcleod/discourse-custom-wizard
 
