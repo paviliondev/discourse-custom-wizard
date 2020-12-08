6dfd9e36c278e3eb0cea9c46694eb3fef6c08e41
Angus McLeod <angus@mcleod.org.au>
2020-12-08 13:27:19 +1100

    FIX: no access locale and svg icon

--- extensions/extra_locales_controller.rb
+++ extensions/extra_locales_controller.rb
@@ -4,8 +4,7 @@ module ExtraLocalesControllerCustomWizard
       return false unless bundle =~ /wizard/ && request.referer =~ /\/w\//
       path = URI(request.referer).path
       wizard_id = path.split('/w/').last
-      wizard = CustomWizard::Wizard.create(wizard_id.underscore, current_user)
-      wizard && wizard.can_access?
+      CustomWizard::Template.exists?(wizard_id.underscore)      
     end
   end
 end
