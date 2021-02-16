c196c4fee787bcdc6119360d1efe67f2fccd9fc0
Faizaan Gagan <fzngagan@gmail.com>
2021-02-16 19:58:43 +0530

    added tests for CustomWizard::RealtimeValidation class

---
+++ spec/components/custom_wizard/realtime_validation_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative '../../plugin_helper'
+
+describe CustomWizard::RealtimeValidation do
+  context 'classes for every validation type exist' do
+    validation_names = CustomWizard::RealtimeValidation.types.keys
+
+    validation_names.each do |name|
+      klass_str = "CustomWizard::RealtimeValidation::#{name.to_s.camelize}"
+
+      it "ensure class for validation: #{name} exists" do
+        expect(klass_str.safe_constantize).not_to be_nil
+      end
+
+      it "#{klass_str} has a perform() method" do
+        expect(klass_str.safe_constantize.instance_methods).to include(:perform)
+      end
+    end
+
+  end
+end
