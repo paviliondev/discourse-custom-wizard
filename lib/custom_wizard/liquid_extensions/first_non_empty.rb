7e695abf07a487aac7aa303bec293583b5592377
Faizaan Gagan <fzngagan@gmail.com>
2021-04-02 11:25:28 +0530

    ran rubocop on changed files

--- lib/custom_wizard/liquid_extensions/first_non_empty.rb
+++ lib/custom_wizard/liquid_extensions/first_non_empty.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module ::CustomWizard
   module LiquidFilter
     module FirstNonEmpty
       def first_non_empty(*multiple)
-        return multiple.find{ |var| var.present? }
+        multiple.find { |var| var.present? }
       end
     end
   end
