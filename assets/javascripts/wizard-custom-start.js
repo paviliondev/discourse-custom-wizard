2788e8026ede53d6df17f62ce1877c2ba6894735
Faizaan Gagan <fzngagan@gmail.com>
2021-04-12 12:05:15 +0530

    ran prettier on stylesheets and global wizard javascripts (#95)

--- assets/javascripts/wizard-custom-start.js
+++ assets/javascripts/wizard-custom-start.js
@@ -1,4 +1,4 @@
-(function() {
-  let wizard = require('discourse/plugins/discourse-custom-wizard/wizard/custom-wizard').default.create();
+(function () {
+  let wizard = require("discourse/plugins/discourse-custom-wizard/wizard/custom-wizard").default.create();
   wizard.start();
 })();
