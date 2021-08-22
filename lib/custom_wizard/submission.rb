1916335fa901dd203587825d5c2ca1744a0b2655
Faizaan Gagan <fzngagan@gmail.com>
2021-08-23 03:00:14 +0530

    FIX: use class method as alias for instance method

--- lib/custom_wizard/submission.rb
+++ lib/custom_wizard/submission.rb
@@ -98,18 +98,18 @@ class CustomWizard::Submission
   end
 
   def self.remove(submission)
-    if submission.present?
-      user_id = submission.user.id
-      wizard_id = submission.wizard.id
-      submission_id = submission.id
+    submission.remove
+  end
+
+  def remove
+    if present?
+      user_id = @user.id
+      wizard_id = @wizard.id
+      submission_id = @id
       data = PluginStore.get("#{wizard_id}_#{KEY}", user_id)
       data.delete_if { |sub| sub["id"] == submission_id }
       PluginStore.set("#{wizard_id}_#{KEY}", user_id, data)
     end
-  end
-
-  def remove
-    self.class.remove(self)
   end
 
   def self.cleanup_incomplete_submissions(wizard)
