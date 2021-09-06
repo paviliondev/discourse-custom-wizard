f6c3c98d7183aed6efc84e22e550af60526414af
angusmcleod <angus@mcleod.org.au>
2021-09-06 17:25:08 +0800

    Add submission serializer spec

--- serializers/custom_wizard/submission_serializer.rb
+++ serializers/custom_wizard/submission_serializer.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 class CustomWizard::SubmissionSerializer < ApplicationSerializer
   attributes :id,
-             :user,
              :fields,
              :submitted_at
 
