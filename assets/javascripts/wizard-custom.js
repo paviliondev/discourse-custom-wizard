61f7cf64aff4e7fffdc31a919ffac1fb94c9b331
angusmcleod <angus@mcleod.org.au>
2021-09-07 11:11:59 +0800

    COMPATIBILITY: add new vendor require

--- assets/javascripts/wizard-custom.js
+++ assets/javascripts/wizard-custom.js
@@ -1,4 +1,6 @@
 //= require_tree_discourse discourse/app/lib
+//= require_tree_discourse discourse/app/mixins
+//
 
 //= require discourse/app/mixins/singleton
 //= require discourse/app/mixins/upload
@@ -66,6 +68,7 @@
 //= require template_include.js
 //= require caret_position.js
 //= require popper.js
+//= require uppy.js
 //= require bootstrap-modal.js
 //= require bootbox.js
 //= require discourse-shims
