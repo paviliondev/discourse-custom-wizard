11192182e7eb2c696a3ee6959d6656eb0d14fc29
Faizaan Gagan <fzngagan@gmail.com>
2021-09-09 19:06:01 +0530

    DEV: bump version according to latest calculation (#141)
    * bump version according to latest calculation
    
    * bumped version for the current PR

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Create custom wizards
-# version: 0.8.1
+# version: 1.13.34
 # authors: Angus McLeod
 # url: https://github.com/paviliondev/discourse-custom-wizard
 # contact emails: angus@thepavilion.io
