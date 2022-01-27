39786689aeb4a7ea1be512a3220116c5ddc175ad
Faizaan Gagan <fzngagan@gmail.com>
2022-01-27 19:54:37 +0530

    bump version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Create custom wizards
-# version: 1.16.3
+# version: 1.16.4
 # authors: Angus McLeod
 # url: https://github.com/paviliondev/discourse-custom-wizard
 # contact emails: angus@thepavilion.io
