ec78229ba055065ec637bb504f58635e48ddbe54
Angus McLeod <angus@mcleod.org.au>
2022-02-09 10:30:46 +1100

    FIX: use request_store properly

--- extensions/tags_controller.rb
+++ extensions/tags_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+require 'request_store'
 
 module CustomWizardTagsController
   def search
-    RequestStore.store[:tag_groups] = params[:tag_groups] if params[:tag_groups].present?
+    ::RequestStore.store[:tag_groups] = params[:tag_groups] if params[:tag_groups].present?
     super
   end
 end
