038604e2845d6e4d74357108cbc4067c1c093a40
Faizaan Gagan <fzngagan@gmail.com>
2022-02-16 19:44:56 +0530

    FIX: checkout correct branches of plugin and discourse (#182)
    * FIX: checkout correct branches of plugin and discourse
    
    * add condition to check correct discourse branch
    
    * FIX: use updated redis version
    
    * bump patch version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Create custom wizards
-# version: 1.18.0
+# version: 1.18.1
 # authors: Angus McLeod
 # url: https://github.com/paviliondev/discourse-custom-wizard
 # contact emails: angus@thepavilion.io
