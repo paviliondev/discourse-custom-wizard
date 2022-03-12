8cac2a596003ee99f11e32e8a7a7fe3095aaa9e3
Angus McLeod <angus@mcleod.org.au>
2022-03-12 14:01:39 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Create custom wizards
-# version: 1.18.3
+# version: 1.18.4
 # authors: Angus McLeod
 # url: https://github.com/paviliondev/discourse-custom-wizard
 # contact emails: angus@thepavilion.io
