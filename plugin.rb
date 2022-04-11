3c0d256e4e2db1d2cee33e35d7c12f941a33888e
Angus McLeod <angus@mcleod.org.au>
2022-04-11 11:35:00 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Create custom wizards for topic creation, onboarding, user surveys and much more.
-# version: 2.0.0.beta
+# version: 2.0.0-beta.1
 # authors: Angus McLeod, Faizaan Gagan, Robert Barrow, Keegan George
 # contact_emails: support@thepavilion.io
 # url: https://github.com/paviliondev/discourse-custom-wizard
