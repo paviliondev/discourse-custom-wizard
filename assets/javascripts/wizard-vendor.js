5d445ecd8640cd19096432efe8bf4f4b6bd2f6d2
Angus McLeod <angus@mcleod.org.au>
2022-07-05 08:45:31 +0200

    COMPATIBILITY: Move all remaining vendor assets into legacy support

--- assets/javascripts/wizard-vendor.js
+++ assets/javascripts/wizard-vendor.js
@@ -2,12 +2,12 @@
 
 //= require legacy/template_include.js
 //= require legacy/uppy.js
-//= require bootstrap-modal.js
-//= require bootbox.js
+//= require legacy/bootstrap-modal.js
+//= require legacy/bootbox.js
 //= require legacy/virtual-dom
 //= require legacy/virtual-dom-amd
 //= require legacy/itsatrap.js
-//= require caret_position.js
+//= require legacy/caret_position.js
 //= require legacy/popper.js
 //= require legacy/tippy.umd.js
 //= require legacy/discourse-shims.js
