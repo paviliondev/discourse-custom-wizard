556a32aa5cb89b252b5e8ca90395a562bbe4811b
Angus McLeod <angus@mcleod.org.au>
2022-07-30 11:23:12 +0100

    wizard_js locales are only loaded on the new /wizard route for non-staff

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Create custom wizards
-# version: 1.22.1
+# version: 1.22.2
 # authors: Angus McLeod
 # url: https://github.com/paviliondev/discourse-custom-wizard
 # contact emails: angus@pavilion.tech
