b2714b524c3ebe38286f29a8b09fc69cf7026d6b
Angus McLeod <angus@mcleod.org.au>
2022-09-02 10:01:21 +0200

    Bump version

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # name: discourse-custom-wizard
 # about: Forms for Discourse. Better onboarding, structured posting, data enrichment, automated actions and much more.
-# version: 1.22.8
+# version: 1.22.9
 # authors: Angus McLeod, Faizaan Gagan, Robert Barrow, Keegan George
 # url: https://github.com/paviliondev/discourse-custom-wizard
 # contact_emails: development@pavilion.tech
