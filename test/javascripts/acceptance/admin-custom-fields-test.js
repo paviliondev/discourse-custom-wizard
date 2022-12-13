0f9fa5fe944423e05c728dae4694abf96ead5fec
jumagura <juangura@gmail.com>
2022-12-13 04:27:41 -0400

    DEV: Add admin custom fields acceptance test

---
+++ test/javascripts/acceptance/admin-custom-fields-test.js
@@ -0,0 +1,349 @@
+import {
+  acceptance,
+  query,
+  visible,
+} from "discourse/tests/helpers/qunit-helpers";
+import { test } from "qunit";
+import { findAll, visit } from "@ember/test-helpers";
+// import selectKit from "discourse/tests/helpers/select-kit-helper";
+
+acceptance("Admin | Custom Fields", function (needs) {
+  needs.user();
+  needs.settings({
+    custom_wizard_enabled: true,
+    available_locales: JSON.stringify([{ name: "English", value: "en" }]),
+  });
+
+  needs.pretender((server, helper) => {
+    server.get("admin/wizards/wizard", () => {
+      return helper.response({
+        wizard_list: [
+          { id: "this_is_testing_wizard", name: "This is testing wizard" },
+        ],
+        field_types: {
+          text: {
+            min_length: null,
+            max_length: null,
+            prefill: null,
+            char_counter: null,
+            validations: null,
+            placeholder: null,
+          },
+          textarea: {
+            min_length: null,
+            max_length: null,
+            prefill: null,
+            char_counter: null,
+            placeholder: null,
+          },
+          composer: {
+            min_length: null,
+            max_length: null,
+            char_counter: null,
+            placeholder: null,
+          },
+          text_only: {},
+          composer_preview: { preview_template: null },
+          date: { format: "YYYY-MM-DD" },
+          time: { format: "HH:mm" },
+          date_time: { format: "" },
+          number: {},
+          checkbox: {},
+          url: { min_length: null },
+          upload: { file_types: ".jpg,.jpeg,.png" },
+          dropdown: { prefill: null, content: null },
+          tag: { limit: null, prefill: null, content: null, tag_groups: null },
+          category: { limit: 1, property: "id", prefill: null, content: null },
+          group: { prefill: null, content: null },
+          user_selector: {},
+        },
+        realtime_validations: {
+          similar_topics: {
+            types: ["text"],
+            component: "similar-topics-validator",
+            backend: true,
+            required_params: [],
+          },
+        },
+        custom_fields: [
+          {
+            id: "external",
+            klass: "category",
+            name: "require_topic_approval",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "category",
+            name: "require_reply_approval",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "category",
+            name: "num_auto_bump_daily",
+            type: "integer",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "category",
+            name: "has_chat_enabled",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "missing uploads",
+            type: "json",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "missing uploads ignored",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "notice",
+            type: "json",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "local_dates",
+            type: "json",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "has_polls",
+            type: "boolean",
+            serializers: null,
+          },
+        ],
+      });
+    });
+    server.get("/admin/wizards", () => {
+      return helper.response({
+        subscribed: false,
+        subscription_type: "none",
+        subscription_attributes: {
+          wizard: {
+            required: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+            permitted: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+            restart_on_revisit: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+          },
+          step: {
+            condition: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+            required_data: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+            permitted_params: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+          },
+          field: {
+            condition: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+            type: {
+              none: [
+                "text",
+                "textarea",
+                "text_only",
+                "date",
+                "time",
+                "date_time",
+                "number",
+                "checkbox",
+                "dropdown",
+                "upload",
+              ],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+            realtime_validations: {
+              none: [],
+              standard: ["*"],
+              business: ["*"],
+              community: ["*"],
+            },
+          },
+          action: {
+            type: {
+              none: [
+                "create_topic",
+                "update_profile",
+                "open_composer",
+                "route_to",
+              ],
+              standard: [
+                "create_topic",
+                "update_profile",
+                "open_composer",
+                "route_to",
+                "send_message",
+                "watch_categories",
+                "add_to_group",
+              ],
+              business: ["*"],
+              community: ["*"],
+            },
+          },
+          custom_field: {
+            klass: {
+              none: ["topic", "post"],
+              standard: ["topic", "post"],
+              business: ["*"],
+              community: ["*"],
+            },
+            type: {
+              none: ["string", "boolean", "integer"],
+              standard: ["string", "boolean", "integer"],
+              business: ["*"],
+              community: ["*"],
+            },
+          },
+          api: {
+            all: { none: [], standard: [], business: ["*"], community: ["*"] },
+          },
+        },
+        subscription_client_installed: false,
+      });
+    });
+    server.get("admin/wizards/custom-fields", () => {
+      return helper.response({
+        custom_fields: [
+          {
+            id: "external",
+            klass: "category",
+            name: "require_topic_approval",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "category",
+            name: "require_reply_approval",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "category",
+            name: "num_auto_bump_daily",
+            type: "integer",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "category",
+            name: "has_chat_enabled",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "missing uploads",
+            type: "json",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "missing uploads ignored",
+            type: "boolean",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "notice",
+            type: "json",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "local_dates",
+            type: "json",
+            serializers: null,
+          },
+          {
+            id: "external",
+            klass: "post",
+            name: "has_polls",
+            type: "boolean",
+            serializers: null,
+          },
+        ],
+      });
+    });
+  });
+
+  test("viewing custom fields tab", async (assert) => {
+    await visit("/admin/wizards/custom-fields");
+    assert.ok(find("table"));
+    assert.ok(findAll("table tbody tr").length === 9);
+    assert.ok(
+      query(".message-content").innerText.includes(
+        "View, create, edit and destroy custom fields"
+      ),
+      "it displays wizard message"
+    );
+    await click(".btn-icon-text");
+    assert.ok(
+      visible(".wizard-subscription-selector"),
+      "custom field class is present"
+    );
+    assert.ok(
+      visible(".wizard-subscription-selector-header"),
+      "custom field type is present"
+    );
+    assert.ok(visible(".input"), "custom field name is present");
+    assert.ok(visible(".multi-select"), "custom field serializer is present");
+    assert.ok(visible(".actions"), "custom field action buttons are present");
+
+    // TODO: Adding a new custom field
+  });
+});
