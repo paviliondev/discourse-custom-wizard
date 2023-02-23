9eb5fc6ff67372ae1edef9a17af734ac0b9c59e1
Angus McLeod <angus@mcleod.org.au>
2023-02-23 19:24:11 +0100

    Complete updates to handle subscription product slugs

--- spec/plugin_helper.rb
+++ spec/plugin_helper.rb
@@ -9,7 +9,6 @@ def get_wizard_fixture(path)
 end
 
 def enable_subscription(type)
-  CustomWizard::Subscription.stubs(:client_installed?).returns(true)
   CustomWizard::Subscription.stubs("#{type}?".to_sym).returns(true)
   CustomWizard::Subscription.any_instance.stubs("#{type}?".to_sym).returns(true)
 end
