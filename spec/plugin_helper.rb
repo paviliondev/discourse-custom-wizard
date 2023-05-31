c256a41668bcb7ab968e7ff4fe91d9764be85bc9
Angus McLeod <angus@mcleod.org.au>
2023-05-31 14:27:00 +0200

    Also ensure subscription client definition is in the right state

--- spec/plugin_helper.rb
+++ spec/plugin_helper.rb
@@ -23,3 +23,12 @@ def disable_subscriptions
     CustomWizard::Subscription.any_instance.stubs("#{type}?".to_sym).returns(false)
   end
 end
+
+def undefine_client_classes
+  Object.send(:remove_const, :SubscriptionClient) if Object.constants.include?(:SubscriptionClient)
+  Object.send(:remove_const, :SubscriptionClientSubscription) if Object.constants.include?(:SubscriptionClientSubscription)
+end
+
+def define_client_classes
+  load File.expand_path("#{Rails.root}/plugins/discourse-custom-wizard/spec/fixtures/subscription_client.rb", __FILE__)
+end
