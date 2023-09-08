c2ffa7699bfda95fb20ea248cedc642e6af45f6b
merefield <merefield@gmail.com>
2023-09-08 10:54:41 +0100

    wip

--- assets/javascripts/discourse/components/wizard-subscription-authorize.js
+++ assets/javascripts/discourse/components/wizard-subscription-authorize.js
@@ -6,19 +6,20 @@ import { action, set } from "@ember/object";
 import { inject as service } from "@ember/service";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
+import { ajax } from "discourse/lib/ajax";
 // import I18n from "I18n";
 
 export default class WizardSubscriptionAuthorize extends Component {
   @service siteSettings;
-  @tracked supplierId = 1;
+  @tracked supplierId = null;
 
   constructor() {
     super(...arguments);
-
+    /admin/plugins/subscription-client/suppliers
   }
 
   @action
   authorize() {
-    window.location.href = `${basePath}/authorize?supplier_id=${this.supplierId}`;
+    window.location.href = `/admin/plugins/subscription-client/authorize?supplier_id=${this.supplierId}`;
   }
 }
