2b867ce678dc5add1eff7c7fca99529eb905c923
merefield <merefield@gmail.com>
2023-10-09 13:39:10 +0100

    FEATURE: add a sub refresh button

--- assets/javascripts/discourse/components/wizard-subscription-badge.js
+++ assets/javascripts/discourse/components/wizard-subscription-badge.js
@@ -1,11 +1,17 @@
 import { inject as service } from "@ember/service";
 import { action, computed } from "@ember/object";
+import { ajax } from "discourse/lib/ajax";
+import { popupAjaxError } from "discourse/lib/ajax-error";
 import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
 import DiscourseURL from "discourse/lib/url";
 import I18n from "I18n";
 
 export default class WizardSubscriptionBadge extends Component {
   @service subscription;
+  @tracked updating = false;
+  @tracked updateIcon = null;
+  basePath = "/admin/plugins/subscription-client";
 
   @computed("subscription.subscriptionType")
   get i18nKey() {
@@ -29,5 +35,27 @@ export default class WizardSubscriptionBadge extends Component {
   @action
   click() {
     DiscourseURL.routeTo(this.subscription.subscriptionLink);
+  }
+
+  @action
+  update() {
+    this.updating = true;
+    return ajax(`${this.basePath}/subscriptions`, {
+      type: "POST",
+    })
+      .then(() => {
+        if (this.subscription.subscribed) {
+          this.updateIcon = "check";
+        } else {
+          this.updateIcon = "times";
+        }
+      })
+      .catch(popupAjaxError)
+      .finally(() => {
+        this.updating = false;
+        // setTimeout(() => {
+        //   this.updateIcon = null;
+        // }, 7000);
+      });
   }
 }
