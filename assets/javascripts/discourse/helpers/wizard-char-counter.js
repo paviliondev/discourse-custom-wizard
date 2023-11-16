df3e5b6494c42089a361bf855a2064423f88bfcc
merefield <merefield@gmail.com>
2023-11-16 16:32:39 +0000

    COMPATIBILITY: resolve register unbound deprecation

---
+++ assets/javascripts/discourse/helpers/wizard-char-counter.js
@@ -0,0 +1,21 @@
+import I18n from "I18n";
+import Handlebars from "handlebars";
+
+export default function wizardCharCounter(body, maxLength) {
+  let bodyLength = body ? body.length : 0;
+  let finalString;
+
+  if (maxLength) {
+    let isOverMax = bodyLength > maxLength ? "true" : "false";
+    finalString = `<div class="body-length" data-length=${bodyLength} data-over-max=${isOverMax}>${bodyLength} / ${I18n.t(
+      "wizard.x_characters",
+      { count: parseInt(maxLength, 10) }
+    )}</div>`;
+  } else {
+    finalString = `<div class="body-length">${I18n.t("wizard.x_characters", {
+      count: parseInt(bodyLength, 10),
+    })}</div>`;
+  }
+
+    return new Handlebars.SafeString(finalString);
+};
