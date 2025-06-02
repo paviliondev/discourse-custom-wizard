a3d5c6c6563380528c02ad73f873e4602692a1b5
Angus McLeod <angus@mcleod.org.au>
2025-06-02 14:37:20 +0200

    COMPATIBILITY: Update TagChooser support.

--- assets/javascripts/discourse/components/custom-wizard-tag-chooser.js
+++ assets/javascripts/discourse/components/custom-wizard-tag-chooser.js
@@ -1,7 +1,32 @@
+import { makeArray } from "discourse/lib/helpers";
 import TagChooser from "select-kit/components/tag-chooser";
 
 export default TagChooser.extend({
-  searchTags(url, data, callback) {
+  search(query) {
+    const selectedTags = makeArray(this.tags).filter(Boolean);
+
+    const data = {
+      q: query,
+      limit: this.siteSettings.max_tag_search_results,
+      categoryId: this.categoryId,
+    };
+
+    if (selectedTags.length || this.blockedTags.length) {
+      data.selected_tags = selectedTags
+        .concat(this.blockedTags)
+        .uniq()
+        .slice(0, 100);
+    }
+
+    if (!this.everyTag) {
+      data.filterForInput = true;
+    }
+    if (this.excludeSynonyms) {
+      data.excludeSynonyms = true;
+    }
+    if (this.excludeHasSynonyms) {
+      data.excludeHasSynonyms = true;
+    }
     if (this.tagGroups) {
       let tagGroupsString = this.tagGroups.join(",");
       data.filterForInput = {
@@ -9,7 +34,10 @@ export default TagChooser.extend({
         groups: tagGroupsString,
       };
     }
-
-    return this._super(url, data, callback);
+    return this.tagUtils.searchTags(
+      "/tags/filter/search",
+      data,
+      this._transformJson
+    );
   },
 });
